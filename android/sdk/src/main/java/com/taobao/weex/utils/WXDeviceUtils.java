98a15562f5b9b242af46554931d77531f3b47f4f
yayun.dyy <yayun.dyy@alibaba-inc.com>
2020-02-24 10:59:24 +0800

    FolderDevice logic
    华为新折叠屏接口适配
    
    Link: https://code.aone.alibaba-inc.com/weex/weex/codereview/2503486
    
    * FolderDevice logic

--- android/sdk/src/main/java/com/taobao/weex/utils/WXDeviceUtils.java
+++ android/sdk/src/main/java/com/taobao/weex/utils/WXDeviceUtils.java
@@ -20,35 +20,83 @@ package com.taobao.weex.utils;
 
 import android.content.Context;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WXDeviceUtils {
 
+    private final static String TAG = "TBDeviceUtils";
+    private final static List<String> HUAWEI_FOLD_DEVICES;
+    private final static String HUAWEI_BRAND = "HUAWEI";
+    private final static String SAMSUNG_BRAND = "samsung";
+
+    static {
+        HUAWEI_FOLD_DEVICES = new ArrayList<String>() {{
+            add("unknownRLI");
+            add("HWTAH");
+            add("MRX-AL09");
+            add("HWMRX");
+            add("TAH-AN00m");
+            add("HWTAH-C");
+            add("RHA-AN00m");
+            add("unknownRHA");
+        }};
+    }
+
+    /**
+     * 折叠屏判断
+     *
+     * @return
+     */
+    public static boolean isFoldDevice() {
+
+        if (SAMSUNG_BRAND.equalsIgnoreCase(Build.BRAND)) {
+            if (TextUtils.equals("SM-F9000", Build.MODEL)) {
+                return true;
+            }
+        }
+
+        if (HUAWEI_BRAND.equalsIgnoreCase(Build.BRAND) && HUAWEI_FOLD_DEVICES.contains(Build.DEVICE)) {
+            WXLogUtils.e(TAG, "is HUAWEI Fold Device");
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 华为折叠屏设备判断
+     */
+    public static boolean isMateX(@NonNull Context context) {
+        if (HUAWEI_BRAND.equalsIgnoreCase(Build.BRAND) && HUAWEI_FOLD_DEVICES.contains(Build.DEVICE)) {
+            WXLogUtils.e(TAG, "is HUAWEI Fold Device");
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 三星Galaxy Fopld
+     */
+    public static boolean isGalaxyFold(@NonNull Context context) {
+
+        if (SAMSUNG_BRAND.equalsIgnoreCase(Build.BRAND) && "SM-F9000".equalsIgnoreCase(Build.MODEL)) {
+            return true;
+        }
+
+        return false;
+    }
 
     public static boolean isAutoResize(Context context){
         if(context == null){
             return false;
         }
-        return isMateX(context) || isGalaxyFold(context);
-    }
-
-    /**
-     * Mate X
-     * */
-    public static boolean isMateX(Context context) {
-        return "HUAWEI".equalsIgnoreCase(Build.BRAND) && ("unknownRLI".equalsIgnoreCase(Build.DEVICE) || ("HWTAH".equalsIgnoreCase(Build.DEVICE)));
-    }
-
-
-    /**
-     * Galaxy Fopld
-     * */
-    public static boolean isGalaxyFold(Context context) {
-
-        if("samsung".equalsIgnoreCase(Build.BRAND) && "SM-F9000".equalsIgnoreCase(Build.MODEL)) {
-            return true;
-        }
-
-        return false;
+        return isFoldDevice() || isMateX(context) || isGalaxyFold(context);
     }
 
 }
